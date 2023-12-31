6afe08b7e8567983d3ea1d8f687f2ed375ebd16d
Mat <m.falkowski@dotmat.de>
2023-12-31 23:27:04 +0100

    partial rollback of char dropdown removal 2519106fe6

--- FurCGui.lua
+++ FurCGui.lua
@@ -163,6 +163,13 @@ FurC.DropdownData = {
     [ver.BASE44] = GetString(SI_FURC_FILTER_VERSION_BASE44_TT),
   },
 
+  ChoicesCharacter = {
+    [1] = GetString(SI_FURC_FILTER_CHAR_OFF),
+  },
+  TooltipsCharacter = {
+    [1] = GetString(SI_FURC_FILTER_CHAR_OFF_TT),
+  },
+
   -- will be set in setupSourceDropdown
   ChoicesSource = {},
   TooltipsSource = {},
@@ -379,7 +386,8 @@ function FurC.ApplyLineTemplate()
   local function resizeDropdowns(controlSize)
     local contRolist = {
       [1] = FurC_DropdownSource,
-      [2] = FurC_DropdownVersion,
+      [2] = FurC_DropdownCharacter,
+      [3] = FurC_DropdownVersion,
     }
     for _, control in pairs(contRolist) do
       control:SetWidth(controlSize)
@@ -400,7 +408,7 @@ function FurC.ApplyLineTemplate()
 
   FurC.SetLineHeight(true)
 
-  local minWidth = 2 * (FurC_DropdownVersion:GetWidth()) + FurC_TypeFilter:GetWidth() + 40
+  local minWidth = 2 * (FurC_DropdownCharacter:GetWidth()) + FurC_TypeFilter:GetWidth() + 40
   local minHeight = 2 * FurCGui_Header:GetHeight()
   FurCGui:SetDimensionConstraints(minWidth, minHeight)
 
@@ -606,6 +614,17 @@ local function createGui()
 
     comboBox:SetSortsItems(false)
 
+    if dropdownName == "Character" then
+      local sortedChars = utils.SortTable(utils.GetCharList(), "name", false)
+      for i, char in ipairs(sortedChars) do
+        table.insert(validChoices, char.name)
+        table.insert(
+          dropdownData["Tooltips" .. dropdownName],
+          zo_strformat(GetString(SI_FURC_STRING_RECIPESFORCHAR), char.name)
+        )
+      end
+    end
+
     for _, val in pairs(validChoices) do
       comboBox:AddItem(comboBox:CreateItemEntry(val, OnItemSelect))
       if val == FurC.GetDropdownChoiceTextual(dropdownName) then
@@ -623,6 +642,7 @@ local function createGui()
   createCraftingTypeFilters()
   createInventoryDropdown("Source")
   createInventoryDropdown("Version")
+  createInventoryDropdown("Character")
   FurC.ChangeTemplateFromButton(FurC.GetTinyUi())
   FurC.SetFontSize(FurC.GetFontSize())
   FurC.LoadFrameInfo()
